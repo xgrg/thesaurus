94d21b88fae786ee2f0f0065f7708844a5a4d47f
Grégory <goperto@gmail.com>
2016-08-23 17:04:28 +0200

    [ENH] ALFAHelper: generate full commandlines given just a subject id...
    ...and a name coding for a step in a pipeline. The parse_command
    function is aware of the different expected filenames templates for
    every step.
    (in the context of ALFA diffusion data processing)
    
    Could be generalized by taking out the variable args_types in an
    external json file.

--- thesaurus.py
+++ thesaurus.py
@@ -4,12 +4,74 @@ import textwrap
 import os.path as osp
 import json
 import os
+import os.path as osp
+
+class ALFAHelper(object):
+    '''Returns filenames from existing items in the ALFA repository
+    or suggests new ones according to the Axon ontology'''
+    def __init__(self, directory='/home/grg/data/ALFA_DWI'):
+        from brainvisa import axon
+        axon.initializeProcesses()
+        import neuroHierarchy
+        self.__db = neuroHierarchy.databases._databases[directory]
+
+    def find_diskitem(self, subject, axontype='Any Type', fmt=None):
+        res = self.__db.findDiskItem(exactType=True, **{'_type': axontype, 'subject':subject})
+        if res is None:
+            if fmt is None:
+                raise Exception('No diskitem found. Provide a format to generate a filename for a new item.')
+            else:
+                res = self.__db.findOrCreateDiskItem(**{'_type': axontype, 'subject':subject, '_format': fmt})
+        return res
+
+    def get_ALFA_types(self):
+        import neuroProcesses
+        return list(set([each.type.name for each in self.__db.findDiskItems(**{'_type': 'Any Type'})]))
+
+    def parse_command(self, subject, name, jsonfile='/home/grg/git/alfa/alfa_dwi_pipeline_aug2016.json'):
+        '''Types starting with @ indicate that the corresponding files must exist (ReadDiskItems). \n
+        Strings starting with # designate hard-coded filenames.
+        Types starting with ! indicate that the filenames will be returned without extension'''
+        j = json.load(open(jsonfile))
+        args_types = {
+            'ants_t1': [ '@ALFA DWI B0 Brain FSL FAST White matter', '@ALFA Denoised Nobias SPM Dilated White matter', 'ALFA ANTS Elast T1 to DWI Transformation Template Filename'],
+            'ants_aal': [ '@ALFA Denoised Nobias T1 Image', '#/home/grg/data/templates/MNI_atlas_templates/MNI_T1.nii', 'ALFA ANTS SyN MNI to T1 Transformation Template Filename'],
+            'warp': [ '@ALFA Denoised Nobias T1 Image', 'ALFA T1 Image Warped to DWI space', '@ALFA DWI B0 Map', '@ALFA ANTS Elast T1 to DWI Transformation', '@ALFA ANTS Elast T1 to DWI Affine Transformation'],
+            'warp_AAL': ['#/home/grg/data/templates/MNI_atlas_templates/aal_MNI_V4.nii', 'ALFA AAL Atlas Warped to T1 space', 'ALFA Denoised Nobias T1 Image', '@ALFA ANTS SyN MNI to T1 Transformation', '@ALFA ANTS SyN MNI to T1 Affine Transformation'],
+            'warp_AAL2DWI': ['@ALFA AAL Atlas Warped to T1 space', 'ALFA AAL Atlas Warped to DWI space', '@ALFA DWI B0 Map', '@ALFA ANTS Elast T1 to DWI Transformation', '@ALFA ANTS Elast T1 to DWI Affine Transformation'],
+            'dtifit': ['@ALFA Denoised Corrected DWI Image', 'ALFA DWI FSL DTIFIT Template Filename', '@ALFA DWI B0 FSL Brain Mask', '@Raw Bvec File', '@Raw Bval File'],
+            'eddycorrect' : ['@ALFA Denoised LPCA DWI Image', 'ALFA Denoised Corrected DWI Image'],
+            'extractb0': ['@ALFA Denoised Corrected DWI Image', 'ALFA DWI B0 Map'],
+            'fslbet.25' : ['@ALFA DWI B0 Map', 'ALFA DWI B0 FSL Masked Brain'],
+            'fslfast': ['!ALFA DWI B0 FSL Masked Brain','ALFA DWI B0 FSL Masked Brain']
+                }
+        types = args_types[name]
+        dsk = []
+        for each in types:
+            t = each.strip('@#!')
+            d = t
+            if not each.startswith('#'):
+                fmt = 'gz compressed NIFTI-1 image' if not 'DTIFIT' in t else 'Directory'
+                d = self.find_diskitem(subject, t, fmt=fmt).fullPath()
+                if each.startswith('@'):
+                    if not osp.isfile(d) and not osp.isdir(d):
+                        raise Exception('%s not found while declared as input (or remove the leading @)'%d)
+                if each.startswith('!'):
+                    d = self.find_diskitem(subject, t, fmt=fmt).fullPath()
+                    d = d[:d.index('.')]
+            dsk.append(d)
+        res = parse_command(j[name], dsk)
+
+        return res
+
+
+
 
 def parse_command(cmd, args):
-    if len(a) != cmd.count('%s'):
+    if len(args) != cmd.count('%s'):
         raise Exception('%s (%s)\n%s\n%s'%(
             args, len(args),
-            j[name],
+            cmd,
             'Please check the number of arguments respect the command.'))
     for each in args:
         cmd = cmd.replace('%s', each, 1)
